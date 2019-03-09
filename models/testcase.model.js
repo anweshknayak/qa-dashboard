998164476f228cd0b8c0fe1e51729a611bef858d
anweshknayak <anweshknayak@gmail.com>
2019-03-09 12:13:36 +0530

    initial commit

---
+++ models/testcase.model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let testCaseSchema = new Schema({
+    testId: {type: Number, required: true},
+    description: {type: String, required: true},
+    status: {type: String, required: true}
+});
